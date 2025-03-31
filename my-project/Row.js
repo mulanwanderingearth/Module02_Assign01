e80479d6a60e65c85c33009ec1c73d64dc13bcfd
mulanwanderingearth <happyxin331@icloud.com>
2025-03-31 13:55:58 -0700

    first draft of flexible rows and columns

--- my-project/Row.js
+++ my-project/Row.js
@@ -1,14 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import styles from "./styles";
-export default function Box({ children }) {
-  return (
-    <View style={styles.box}>
-      <Text style={styles.boxText}>{children}</Text>
-    </View>
-    );
+
+export default function Row({ children }) {
+  return <View style={styles.row}>{children}</View>;
 }
-Box.propTypes = {
-   children: PropTypes.node.isRequired,
+
+Row.propTypes = {
+  children: PropTypes.node.isRequired,
 };
